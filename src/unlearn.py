4f66d30f7bf6c943c580bdc5a3805717d0e9f0c3
TsukasaYatoki <Oumaekumiko@hnu.edu.cn>
2025-07-17 20:30:00 +0800

    update

--- src/unlearn.py
+++ src/unlearn.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import torch
 import random
 
-def unlearn(model, train_loader, num_epochs=50, learning_rate=0.0001, device='cuda'):
+def unlearn(model, train_loader, num_epochs=20, learning_rate=0.001, device='cuda'):
     model.to(device)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-4)
@@ -34,17 +34,15 @@ def main():
     assert(torch.cuda.is_available()), "CUDA is not available."
     
     transform = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
     ])
     dataset = CIFAR10(root='./dataset', train=True, transform=transform)
-    subset_ratio = 0.01  # 使用1%的干净训练数据
+    subset_ratio = 0.05 # 使用5%的干净训练数据
     subset_size = int(len(dataset) * subset_ratio)
     subset_indices = random.sample(range(len(dataset)), subset_size)
     subset = Subset(dataset, subset_indices)
-    data_loader = DataLoader(subset, batch_size=128, shuffle=True, num_workers=4)
+    data_loader = DataLoader(subset, batch_size=64, shuffle=True, num_workers=4)
     
     # 加载后门模型
     model = ResNet18()
